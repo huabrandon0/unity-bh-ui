8ed33d63605073cd1ad8f949a8940b14f4755db2
Brandon Hua <huabrandon0@gmail.com>
2018-12-17 23:13:40 -0800

    Added menu transitions in example scene

--- Assets/BH.UI/Scripts/Runtime/UIAnimatedElement.cs
+++ Assets/BH.UI/Scripts/Runtime/UIAnimatedElement.cs
@@ -5,8 +5,26 @@ namespace BH.UI
     public abstract class UIAnimatedElement : MonoBehaviour
     {
         [SerializeField] protected UIAnimatedElementSettings _animatedElementSettings;
+        public UIAnimatedElementSettings AnimatedElementSettings
+        {
+            get { return _animatedElementSettings; }
+            private set { }
+        }
+
         [SerializeField] protected float _enterDelay = 0f;
+        public float EnterDelay
+        {
+            get { return _enterDelay; }
+            private set { }
+        }
+
         [SerializeField] protected float _exitDelay = 0f;
+        public float ExitDelay
+        {
+            get { return _exitDelay; }
+            private set { }
+        }
+
         protected bool _isAnimating = false;
 
         public abstract void Enter();
--- Assets/BH.UI/Scripts/Runtime/UIAnimatedElementOverlay.cs
+++ Assets/BH.UI/Scripts/Runtime/UIAnimatedElementOverlay.cs
@@ -1,27 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace BH.UI
 {
     public class UIAnimatedElementOverlay : MonoBehaviour
     {
-        List<UIAnimatedElement> _uiElementAnimators;
+        List<UIAnimatedElement> _uiAnimatedElements;
+
+        float _enterDuration = 0f;
+        float _exitDuration = 0f;
+
+        public delegate void NoArgDelegate();
 
         void Awake()
         {
-            _uiElementAnimators = new List<UIAnimatedElement>(GetComponentsInChildren<UIAnimatedElement>());
+            _uiAnimatedElements = new List<UIAnimatedElement>(GetComponentsInChildren<UIAnimatedElement>());
+            _enterDuration = (from x in _uiAnimatedElements select x.AnimatedElementSettings._enterDuration + x.EnterDelay).Max();
+            _exitDuration = (from x in _uiAnimatedElements select x.AnimatedElementSettings._exitDuration + x.ExitDelay).Max();
         }
 
         public void Enter()
         {
-            foreach (UIAnimatedElement uiElementAnimator in _uiElementAnimators)
+            foreach (UIAnimatedElement uiElementAnimator in _uiAnimatedElements)
                 uiElementAnimator.Enter();
         }
-
+        
+        public IEnumerator Enter(NoArgDelegate callback)
+        {
+            Enter();
+            yield return new WaitForSeconds(_enterDuration);
+            callback.Invoke();
+        }
+        
         public void Exit()
         {
-            foreach (UIAnimatedElement uiElementAnimator in _uiElementAnimators)
+            foreach (UIAnimatedElement uiElementAnimator in _uiAnimatedElements)
                 uiElementAnimator.Exit();
+        }
+
+        public IEnumerator Exit(NoArgDelegate callback)
+        {
+            Exit();
+            yield return new WaitForSeconds(_exitDuration);
+            callback.Invoke();
         }
     }
 }
